1ff709bb7042ae6ac89198e2c8990435454faf97
PRANAV BHATT <pbhatt@pbhatt-ltm1.internal.salesforce.com>
2019-07-09 00:02:34 -0400

    why

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var pg = require('pg');
-var fetch = require('node-fetch');
+//var fetch = require('node-fetch');
 
 var app = express();
 
